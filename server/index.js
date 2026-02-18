4476e4a7f1661e9b2104ff0f945af603dda383ab
lakshmi.k <chandanshettyk85@gmail.com>
2026-02-19 00:23:34 +0530

    fix render port

--- server/index.js
+++ server/index.js
@@ -10,9 +10,9 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
-  },
+  }
 });
 
 io.on("connection", (socket) => {
@@ -30,12 +30,13 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
-    console.log("User Disconnected:", socket.id);
+    console.log("user disconnected:", socket.id);
   });
-});
+  });
 
-server.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+ const PORT = process.env.PORT || 5000;
 
+server.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
+});
 
